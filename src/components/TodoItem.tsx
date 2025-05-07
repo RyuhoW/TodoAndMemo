2aa418d63ee6ac7aadaa4373a45f6f980a596567
Ryuho <ryuho.w0610@gmail.com>
2025-05-07 19:57:00 +0900

    style: improve delete button visibility with hover effects and tooltips

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -32,7 +32,8 @@ const TodoItem: React.FC<TodoItemProps> = memo(({ todo, onToggle, onDelete, onUp
         <div className="flex gap-2">
           <button
             onClick={() => setIsEditingMemo(true)}
-            className="text-indigo-600 hover:text-indigo-700 transition-colors duration-200"
+            className="p-2 text-indigo-600 hover:text-indigo-700 hover:bg-indigo-50 rounded-full transition-colors duration-200"
+            title="メモを編集"
           >
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
               <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
@@ -40,7 +41,8 @@ const TodoItem: React.FC<TodoItemProps> = memo(({ todo, onToggle, onDelete, onUp
           </button>
           <button
             onClick={() => onDelete(todo.id)}
-            className="text-red-600 hover:text-red-700 transition-colors duration-200"
+            className="p-2 text-red-600 hover:text-red-700 hover:bg-red-50 rounded-full transition-colors duration-200"
+            title="タスクを削除"
           >
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
