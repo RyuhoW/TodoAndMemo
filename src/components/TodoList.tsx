979ae1dec9b70ef6889c1b85769f5254ac4853d1
Ryuho <ryuho.w0610@gmail.com>
2025-05-07 20:24:28 +0900

    refactor: optimize React components with memo and callbacks

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,24 +1,37 @@
-import React, { memo } from 'react';
+import React, { memo, useCallback } from 'react';
 import { TodoList as TodoListType } from '../types/todo';
 import TodoItem from './TodoItem';
+import { Todo } from '../types/todo';
 
 interface TodoListProps {
-  todos: TodoListType;
+  todos: Todo[];
   onToggle: (id: number) => void;
   onDelete: (id: number) => void;
   onUpdateMemo: (id: number, memo: string) => void;
 }
 
 const TodoList: React.FC<TodoListProps> = memo(({ todos, onToggle, onDelete, onUpdateMemo }) => {
+  const handleToggle = useCallback((id: number) => {
+    onToggle(id);
+  }, [onToggle]);
+
+  const handleDelete = useCallback((id: number) => {
+    onDelete(id);
+  }, [onDelete]);
+
+  const handleUpdateMemo = useCallback((id: number, memo: string) => {
+    onUpdateMemo(id, memo);
+  }, [onUpdateMemo]);
+
   return (
     <div className="space-y-3 animate-fade-in">
       {todos.map((todo) => (
         <TodoItem
           key={todo.id}
           todo={todo}
-          onToggle={onToggle}
-          onDelete={onDelete}
-          onUpdateMemo={onUpdateMemo}
+          onToggle={handleToggle}
+          onDelete={handleDelete}
+          onUpdateMemo={handleUpdateMemo}
         />
       ))}
     </div>
